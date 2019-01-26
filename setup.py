5a04e3cc42846033d3fe2e967307a22a2b73c4e8
CodeDojo Miyoshi <46787653+dojo-miyoshi@users.noreply.github.com>
2019-01-26 16:17:53 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,13 @@ for env in os.environ:
 print('--------------------')
 print(os.environ.get('EV'))
 print(os.environ.get('EX'))
+print(os.environ.get('GH_TOKEN'))
+print(os.environ.get('TRAVIS_REPO_SLUG'))
+
+ev=os.environ.get('EV')
+ex=os.environ.get('EX')
+print("{0} {1}".format(ev,ex))
+
+# https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
 
 print("setup")
