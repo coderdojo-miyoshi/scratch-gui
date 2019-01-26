31e64f058cdb127deba8046bcced8bcee7887278
CodeDojo Miyoshi <46787653+dojo-miyoshi@users.noreply.github.com>
2019-01-26 16:26:57 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,11 @@ import os
 import git
 #for env in os.environ:
 #    print(env)
-print('--------------------')
-print(os.environ.get('EV'))
-print(os.environ.get('EX'))
-print(os.environ.get('GH_TOKEN'))
-print(os.environ.get('TRAVIS_REPO_SLUG'))
+#print('--------------------')
+#print(os.environ.get('GH_TOKEN'))
+#print(os.environ.get('TRAVIS_REPO_SLUG'))
 
-ev=os.environ.get('EV')
-ex=os.environ.get('EX')
 travis_repo_slug=os.environ.get('TRAVIS_REPO_SLUG')
-
-print("https://{0}@github.com/{1}.git".format(ev,travis_repo_slug))
+gh_token=os.environ.get('GH_TOKEN')
+print("https://{0}@github.com/{1}.git".format(gh_token,travis_repo_slug))
 
