6304012896fee699c329f194298872aad612611e
CodeDojo Miyoshi <46787653+dojo-miyoshi@users.noreply.github.com>
2019-01-29 14:02:48 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ gh_token=os.environ.get('GH_TOKEN')
 url="https://{0}@github.com/{1}.git".format(gh_token,travis_repo_slug)
 _repo_path = os.path.join('./', 'repo')
 print(_repo_path)
-
+print(dir(os.listdir(_repo_path)))
 #repo = git.Repo.init(_repo_path)
 #repo.create_remote( 'origin', url )
 #repo.git.checkout('develop', b='build')
