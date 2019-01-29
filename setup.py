7a8fe60668841489b002038a22303a6f61190d1b
CodeDojo Miyoshi <46787653+dojo-miyoshi@users.noreply.github.com>
2019-01-29 14:18:48 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,6 @@ travis_repo_slug=os.environ.get('TRAVIS_REPO_SLUG')
 gh_token=os.environ.get('GH_TOKEN')
 url="https://{0}@github.com/{1}.git".format(gh_token,travis_repo_slug)
 _repo_path = os.path.join('./', 'repo')
-print(_repo_path)
-#repo = git.Repo.init(_repo_path)
 repo = git.Repo.clone_from(url, _repo_path,branch='develop')
+repo.git.push( 'origin','build' )
 print(os.listdir(_repo_path))
-
-#repo.create_remote( 'origin', url )
-#repo.git.checkout('develop', b='build')
-#repo.git.push( 'origin','build' )
-
