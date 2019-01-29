a01bde66096c5ff72193e14c90ca0519268d582c
CodeDojo Miyoshi <46787653+dojo-miyoshi@users.noreply.github.com>
2019-01-29 14:51:58 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import os
 import git
 #for env in os.environ:
 #    print(env)
-
 travis_repo_slug=os.environ.get('TRAVIS_REPO_SLUG')
 gh_token=os.environ.get('GH_TOKEN')
 url="https://{0}@github.com/{1}.git".format(gh_token,travis_repo_slug)
@@ -10,4 +9,3 @@ _repo_path = os.path.join('./', 'repo')
 repo = git.Repo.clone_from(url, _repo_path,branch='develop')
 repo.git.checkout('-b', 'build')
 repo.git.push( 'origin','build' )
-print(os.listdir(_repo_path))
